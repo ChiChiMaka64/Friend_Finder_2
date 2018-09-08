3a548bdaa1baf39e9ecdada9f34699aac77936b7
Lynette Okoroike <lcokoroike@gmail.com>
2018-09-08 12:27:42 -0700

    Changes

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,6 +1,12 @@
 var futureCastInfo=[
-    {
-        
-    },
+    { name: "Lynette Okoroike",
+      picture: "",
+      answers: [ 5, 2, 5, 1, 5, 3, 4, 3, 5, 5]
+     },
+     {
+       name: "Tracy Williams",
+       picture: "",
+       answers: [5, 5, 4, 3, 4, 3, 2, 1, 2, 1]
+     }
 ];
 module.exports=futureCastInfo;
