cfd8f749c145ade37efaec5767ddc65b28d1bd42
Lynette Okoroike <lcokoroike@gmail.com>
2018-10-12 13:57:45 -0700

    update.

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 require("./app/routing/apiRoutes");
 require("./app/routing/htmlRoutes");
+require("./app/data/friends");
 
 var app = express();
 
@@ -9,14 +10,13 @@ var app = express();
 // process.env.PORT lets the port be set by Heroku
 var PORT = process.env.PORT || 8080;
 
-
+app.get("/all")
 
 // Parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static("public"));
-// app.use(data);
-// app.use(routes);
+
 
 
 
