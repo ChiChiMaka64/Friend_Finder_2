f1b937ab2c7d0ff3364229d886a4ad4161dc4c1e
Lynette Okoroike <lcokoroike@gmail.com>
2018-10-22 19:38:43 -0700

    Almost There.

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,13 +1,53 @@
-var castData=[
+var castData = [
     {
         name: "Lynette Okoroike",
         picture: "",
-        answers: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5 ]
+        answers: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
     },
     {
-      name: "Tracy Williams",
-      picture: "",
-      answers: [4, 4, 4, 4, 4, 4, 4, 4, 4, 4]
-    }
+        name: "Tracy Williams",
+        picture: "",
+        answers: [4, 4, 4, 4, 4, 4, 4, 4, 4, 4]
+    },
+    {
+        name: "Collins Jackson",
+        picture: "",
+        answers: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+    },
+    {
+        name: "Basil Green",
+        picture: "",
+        answers: [2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
+    },
+    {
+        name: "Cynthia Felter",
+        picture: "",
+        answers: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    },
+    {
+        name: "Simeon Marks",
+        picture: "",
+        answers: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+    },
+    {
+        name: "Lizzie McGuire",
+        picture: "",
+        answers: [4, 4, 4, 4, 4, 4, 4, 4, 4, 4]
+    },
+    {
+        name: "Ren Stevens",
+        picture: "",
+        answers: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+    },
+    {
+        name: "Kim Possible",
+        picture: "",
+        answers: [2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
+    },
+    {
+        name: "Alexis Texas",
+        picture: "",
+        answers: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    },
 ];
-module.exports=castData;
+module.exports = castData;
--- server.js
+++ server.js
@@ -17,8 +17,47 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static("public"));
 
+app.post('/api/friends', function (req, res) {
+  // grabs the user's input from the survey
+  let user = req.body;
 
+  // takes the user's scores from each question
+  let userScore = user.scores;
 
+  // variables used to hold match's name and image
+  let matchName = '';
+  let matchPic = '';
+
+  // setting initial value high for the comparison
+  let friendDif = 1000000;
+
+  // for loop that goes through all friends on friends.js
+  for (let i = 0; i < friends.length; i++) {
+
+      // sets initial difference to 0
+      let difference = 0;
+
+      // for loop that compares the difference between the user and friends list scores
+      for (let j = 0; j < userScore.length; j++) {
+          difference += Math.abs(friends[i].scores[j] - userScore[j])
+      }
+
+      // whichever friend has the lowest difference will become the match
+      if (difference < friendDif) {
+
+          friendDif = difference;
+          matchName = friends[i].name;
+          matchPic = friends[i].photo;
+      }
+  }
+
+  // pushes user's input to the friendslist
+  friends.push(user);
+
+  // returns the appropriate data for the modal on the survey 
+  res.json({ status: 'OK', matchName: matchName, matchPic: matchPic });
+  
+})
 
 app.listen(PORT, function() {
     
